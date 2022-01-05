5cb45ec5533cac9d73602380f37b5f7b56a621f5
bogdan <bogdan4888@gmail.com>
2022-01-05 10:47:31 +0200

    Added interface with impl

---
+++ grails-app/services/bogdan/impl/PersonServiceImpl.groovy
@@ -0,0 +1,42 @@
+package bogdan.impl
+
+import bogdan.PersonService
+import com.bogdan.Person
+import grails.transaction.Transactional
+import org.grails.web.json.JSONObject
+
+@Transactional
+class PersonServiceImpl implements PersonService {
+
+    List<Person> list() {
+        return Person.findAll()
+    }
+
+    Person getOne(def params) {
+        return Person.findById(params?.id)
+    }
+
+    Person save(def request) {
+        JSONObject personJson = request.JSON
+        Person personInstance = new Person(personJson)
+
+        personInstance = personInstance.save()
+
+        return personInstance
+    }
+
+    Person update(def params, def request) {
+        def personJson = request.JSON
+        Person personInstance = Person.get(params?.id)
+
+        personInstance.properties = personJson
+        personInstance = personInstance.merge()
+
+        return personInstance
+    }
+
+    void delete(def params) {
+        Person personInstance = Person.get(params?.id)
+        personInstance.delete()
+    }
+}
